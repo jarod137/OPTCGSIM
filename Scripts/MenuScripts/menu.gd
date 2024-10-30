31a7c4ee12fb871f40f064a5349eda8e1f1dfcdf
Alex <78886791+alexander2475@users.noreply.github.com>
2024-10-30 14:19:43 -0600

    Started messing with deckbuilder. waiting on fix for card images and jpg issue

--- Scripts/MenuScripts/menu.gd
+++ Scripts/MenuScripts/menu.gd
@@ -3,14 +3,11 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Game/World.tscn")
 
-
 func _on_deck_builder_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://Scenes/DeckBuilder/DeckBuilder.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Options/options_menu.tscn")
-
 
 func _on_quit_pressed():
 	get_tree().quit()
