72dd7573cf1e3d4b417cb5745d489356323a73e9
Alex <78886791+alexander2475@users.noreply.github.com>
2024-11-04 13:11:47 -0700

    Working on deckbuilder screen. Had to delete and start new as old scene file was corrupted.

--- Scripts/DeckBuilderScripts/DeckBuilder.gd
+++ Scripts/DeckBuilderScripts/DeckBuilder.gd
@@ -1,9 +1,11 @@
 extends Node
 
 var json = JSON.new()
-var path = "res://Assets/CardData/cards.json"
-
+var path = "res://Assets/CardData/data.json"
 var card_data = []
+
+var current_page = 0
+var cards_per_page = 25
 
 func _ready():
 	load_card_data()
@@ -12,7 +14,6 @@ func _ready():
 func _process(delta):
 	pass
 	
-
 func load_card_data():
 	var fileExists = FileAccess.file_exists(path)
 	
@@ -31,25 +32,55 @@ func load_card_data():
 	else:
 		print("Error: JSON file not found")
 		
-func populate_UI():
-	var deckBuilderScene = preload("res://Scenes/DeckBuilder/DeckBuilder.tscn")
-	
-	var grid = $CanvasLayer/GridContainer
-	
-	if !grid:
-		print("Error: Existing GridContainer not found in scene")
-		return
-	
-	grid.add_theme_constant_override("h_separation", 20)
-	grid.add_theme_constant_override("v_separation", 20)
-	
-	for card_info in card_data:
-		var card_instance = deckBuilderScene.instantiate()
-	
-		var name_node = card_instance.get_node_or_null("CanvasLayer/GridContainer/Control/character_name")
-		if name_node:
-			name_node.text = card_info["name"]
-		else:
-			print("character_name node not found - check the path: CanvasLayer/GridContainer/Control/character_name")
 		
-		grid.add_child(card_instance)
+func populate_UI(min_index: int = 0, max_index: int = 25):
+	var deckBuilderScene = preload("res://Scenes/DeckBuilder/DeckBuilding.tscn")
+	var cardScene = preload("res://Scenes/DeckBuilder/DeckBuildContainer.tscn")
+	
+	var deck_instance = deckBuilderScene.instantiate()
+	
+	var canvas_layer = deck_instance.get_node_or_null("CanvasLayer")
+	if not canvas_layer:
+		print("Error: CanvasLayer not found")
+		return
+		
+	var grid_container = canvas_layer.get_node_or_null("GridContainer")
+	if not grid_container:
+		print("Error: GridContainer not found")
+		return
+		
+	for child in grid_container.get_children():
+		child.queue_free()
+	
+	for i in range(min_index, min(max_index, card_data.size())):
+		var card_info = card_data[i]
+		var card_instance = cardScene.instantiate()
+		
+		var sprite_node = card_instance.get_node("cardImages")
+		if sprite_node:
+			var img_path = card_info.get("imgPath", "")
+			if img_path != "":
+				var texture = load(img_path) as Texture2D
+				sprite_node.texture = texture
+		
+		grid_container.add_child(card_instance)
+		
+	add_child(deck_instance)
+
+func _on_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_mouse_exited():
+	pass # Replace with function body.
+
+# Next page button hit
+func _on_button_pressed():
+	current_page += 1
+	var min_index = current_page * cards_per_page
+	var max_index = min_index + cards_per_page
+	populate_UI(min_index, max_index)
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
