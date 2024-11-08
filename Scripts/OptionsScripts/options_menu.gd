a5b45f54ac17a2d4579c051bfe3c045336b6bb4f
David Kuntz <123694850+davidkuntz02@users.noreply.github.com>
2024-11-08 13:57:23 -0700

    network menu added

--- Scripts/OptionsScripts/options_menu.gd
+++ Scripts/OptionsScripts/options_menu.gd
@@ -5,3 +5,6 @@ func _on_volume_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
+
+func _on_network_pressed() -> void:
+	get_tree().change_scene_to_file("res://Netorking/network.tscn")
