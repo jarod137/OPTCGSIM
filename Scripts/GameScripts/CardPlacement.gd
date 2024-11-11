1fa3a1347d8213ef8d862963e84939dec1a822d7
Jared K. <111449156+jarod137@users.noreply.github.com>
2024-11-11 11:23:09 -0700

    started making player 2

--- Scripts/GameScripts/CardPlacement.gd
+++ Scripts/GameScripts/CardPlacement.gd
@@ -19,5 +19,5 @@ func placeCard():
 	var projectResolutionHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
 	#add an if card is character, event, or stage here
 	count += 120
-	cardTemp.global_position = Vector2(projectResolution/4 + count, projectResolutionHeight/2 - 200) - self.position*2
+	cardTemp.global_position = Vector2(projectResolution/4 + count, projectResolutionHeight/2 + 200) - self.position*2
 	add_child(cardTemp)
