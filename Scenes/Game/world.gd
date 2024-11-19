4130aec4df6f2697850797c517359f80104c5853
Jared K. <111449156+jarod137@users.noreply.github.com>
2024-11-18 22:02:45 -0700

    .

--- Scenes/Game/world.gd
+++ Scenes/Game/world.gd
@@ -4,16 +4,13 @@ var turncount = 0
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
-	$ButtonSound.play()
 
 
 func _on_end_turn_button_pressed() -> void:
-	$ButtonSound.play()
 	$UI/Player1/DonDeck.donDraw()
 
 
 func _on_start_turn_button_pressed() -> void:
-	$ButtonSound.play()
 	if turncount == 0:
 		$UI/Player1/DonDeck.setUpDon()
 	turncount += 1
@@ -21,12 +18,10 @@ func _on_start_turn_button_pressed() -> void:
 
 
 func _on_win_pressed() -> void:
-	$ButtonSound.play()
 	get_tree().change_scene_to_file("res://victory.tscn")
 
 
 func _on_lose_pressed() -> void:
-	$ButtonSound.play()
 	get_tree().change_scene_to_file("res://Loss.tscn")
 
 
