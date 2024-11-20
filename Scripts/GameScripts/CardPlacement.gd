bd4bbb715ae6bc805a16a0c2c283bc5cd93cc76e
Jared K <jarod137@nmsu.edu>
2024-11-19 21:07:04 -0700

    i think i fixed the things i broke

--- Scripts/GameScripts/CardPlacement.gd
+++ Scripts/GameScripts/CardPlacement.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var cardCount = 0
 
 var count: int
 @onready var card = preload("res://Scenes/Game/cardonBoard.tscn")
@@ -12,18 +13,7 @@ func _on_mouse_exited():
 	Game.mouseOnPlacement = false
 
 
-
 func placeCard():
-<<<<<<< HEAD
-	var cardTemp = card.instantiate()
-	var projectResolution = ProjectSettings.get_setting("display/window/size/viewport_width")
-	var projectResolutionHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
-	#add an if card is character, event, or stage here
-	count += 120
-	cardTemp.global_position = Vector2(projectResolution/4 + count, projectResolutionHeight/2 - 240) - self.position*2
-	add_child(cardTemp)
-=======
-	var cardCount = 0
 	if cardCount < 5:
 		cardCount += 1
 		var cardTemp = card.instantiate()
@@ -33,4 +23,3 @@ func placeCard():
 		count += 120
 		cardTemp.global_position = Vector2(projectResolution/4 + count, projectResolutionHeight/2 - 240) - self.position*2
 		add_child(cardTemp)
->>>>>>> parent of 27e088c (small changes)
