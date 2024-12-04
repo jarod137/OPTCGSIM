7053574943b25b04d144b9500482ed8f21161103
Jared K. <111449156+jarod137@users.noreply.github.com>
2024-12-04 04:23:37 -0700

    made it so cards on board tap when left clicked

--- Scripts/GameScripts/cardonBoard.gd
+++ Scripts/GameScripts/cardonBoard.gd
@@ -1,41 +1,22 @@
 extends Container
 
+var _active = true
 
-var _active = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 func _battle():
-	# how I plan to implement battling
 	pass
-	
-	#if power < opponentPower:
-		#pass
-	#else:
-		#if isLeader:
-			#takeLife()
-		#else:
-			#opponentCard.trash()
-	#pass
-
-func _on_mouse_entered() -> void:
-	_active = true
-	show_attack_menu()
 
 
-func _on_mouse_exited() -> void:
-	_active = false
-	hide_attack_menu
-
-# Show the popup menu
-func show_attack_menu():
-	if _active:
-		var popup = get_node("res://Scenes/Game/CardMenu.tscn")
-		#if popup:
-		popup.popup()  # Show the popup menu
-
-# Hide the popup menu
-func hide_attack_menu():
-	$"AttackMenu".hide()
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			print("Container clicked!")
+			if _active == true:
+				$Anim.play("tap")
+				_battle()
+				_active = false
+			else:
+				$Anim.play("untap")
+				_active = true
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			self.free()
